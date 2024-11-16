6e68ed66c6cd9106b87c99be20ca3f119a7901fe
niharikaareddy <niharikaareddy03@gmail.com>
2024-11-16 16:35:23 +0530

    l1

---
+++ lesson 1/python 1 .py
@@ -0,0 +1,8 @@
+name="niharika"
+age="14"
+school="st.joseph's"
+clas="9"
+print(f"my name is {name}")
+print (f"and my is {age}")
+print (f"I study in the school {school}")
+print (f"in class {clas}")
